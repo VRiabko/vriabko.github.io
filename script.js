f8516dd77197cb4c364d3bbf4846fda540a82219
VRiabko <varvara2391@gmail.com>
2022-09-28 20:05:10 +0200

    load fix

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("hello")
+document.querySelectorAll(".project")
+    .forEach(project => {
+        // console.log(project)
+        project.onclick = () => {
+            document.querySelector('.display h3').innerText = project.querySelector('h3').innerText
+            document.querySelector('.display p').innerText = project.querySelector('p').innerText
+            document.querySelector('.display img').src = project.querySelector('img').src
+            document.querySelector('.display a').href = project.querySelector('a').href
+        }
+    }
+)
+    document.querySelector('.project').click()
